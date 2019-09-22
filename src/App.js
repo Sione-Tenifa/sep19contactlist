c296d48269c50cf7e87b92acb66b874c7c30ab04
Sione Tenifa <johntenifa@yahoo.com>
2019-09-21 18:06:09 -0600

    add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Header, } from "semantic-ui-react";
 
 class App extends Component {
+  state = { 
+    contacts: [
+      { id: 1, firstName: "Jerry", phone: "801-121-5656", },
+      { id: 2, firstName: "George", phone: "626-567-1872", },
+      { id: 3, firstName: "Cosmo", phone: "802-134-9876", },
+    ],
+  };
   render() {
     return (
-      <div>
-        <h1>React Contact List</h1>
-      </div>
+      <Container>
+        <Header as="h1">React Contact List</Header>
+      </Container>
     );
   }
 }
